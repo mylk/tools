b755adf0cabdfe6be0f06e6f8cda569a475ffe5f
Kostas Milonas <milonas.ko@gmail.com>
2020-10-15 22:25:01 +0300

    changes data source in script for today's covid-19 cases in greece

--- cases.sh
+++ cases.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-TODAY=`command date '+%Y-%m-%d'`
+
+TODAY=`command date '+%d/%m/%Y'`
 # prefixed with "command" to override aliases of date command (ex. defined in .bashrc)
 
-curl 'https://covidapi.ismood.com/daily-info/?country_name=greece' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0' -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: https://covid19live.ismood.com/' -H 'Authorization: Basic ZmFkaWw6aXNjb3Y0NTZA' -H 'Origin: https://covid19live.ismood.com' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -s | jq ".[] | select(.date == \"$TODAY\")[\"date\", \"new_cases\"]"
+echo "${TODAY}"
+
+curl -s --compressed 'https://flo.uri.sh/visualisation/3334247/embed?auto=1' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0' -H 'Referer: https://public.flourish.studio/visualisation/3334247/' -H 'DNT: 1' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' | \
+grep "_Flourish_data = " | \
+awk -F "= " '{print($2)}' | \
+sed 's/.$//' | \
+jq -r ".[][] | select(.label == \"${TODAY}\")[\"value\"][]"
