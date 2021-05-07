d669722b15c0137c2f5d90ad4e4db5366c7321f3
Kostas Milonas <milonas.ko@gmail.com>
2021-05-07 10:26:11 +0300

    improves revolut-has.sh to allow multiple ticker verification

--- revolut-has.sh
+++ revolut-has.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-results=`wget -q -O - "https://raw.githubusercontent.com/nmapx/revolut-stocks-list/master/LIST.md" | awk '{print($2)}' | grep -iE "^$1$"`
+# get the list of tickers, keep the lines starting with a character, keep all lines except from the first 2, keep the second column
+results=`wget -q -O - "https://raw.githubusercontent.com/nmapx/revolut-stocks-list/master/LIST.md" | grep ^\| | tail -n+3 | awk '{print($2)}'`
 
-if [ -z "$results" ]; then exit 1; fi
+# split the input by the delimiter
+tickers=($(echo $1 | tr "," "\n"))
 
-echo "Yeap!"
+# check if each ticker exists
+for ticker in "${tickers[@]}"
+do
+    exists=`echo "$results" | grep -iE "^$ticker$"`
+
+    if [ ! -z "$exists" ]; then
+        # echo and convert to upper case
+        echo "${ticker^^} exists!"
+    fi;
+done
+
 exit 0
 
