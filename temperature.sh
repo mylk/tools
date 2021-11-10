3b207bde1648db1a414b68b22805810f0edab114
Kostas Milonas <milonas.ko@gmail.com>
2021-11-10 19:58:52 +0200

    modifies temperature.sh to parse the new interface of the weather station

--- temperature.sh
+++ temperature.sh
@@ -3,6 +3,6 @@
 # get your local meteo station url from http://meteo.gr/Gmap.cfm
 STATION_URL="http://penteli.meteo.gr/stations/faliro/"
 
-TEMPERATURE=`wget "$STATION_URL" -O - -o /dev/null | grep --text "3366FF" | head -1 | awk -F ">" '{print($5)}' | awk -F "<" '{print($1)}' | iconv -f WINDOWS-1253 -t UTF8`
+TEMPERATURE=`wget "$STATION_URL" -O - -o /dev/null | grep -m1 "lright" | awk -F ">" '{print($3)}' | awk -F "<" '{print($1)}'`
 
 echo $TEMPERATURE;
